0b8d1b68b0a3382c4c201775b9031cfe569d9b80
taiwan-brown-bear <charles.liu.0857@gmail.com>
2025-06-18 09:55:30 -0700

    step_1

---
+++ src/main/java/com/taiwan_brown_bear/llm_evaluate/LlmEvaluateApplication.java
@@ -0,0 +1,13 @@
+package com.taiwan_brown_bear.llm_evaluate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmEvaluateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LlmEvaluateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taiwan_brown_bear/llm_evaluate/LlmEvaluateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taiwan_brown_bear.llm_evaluate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlmEvaluateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
