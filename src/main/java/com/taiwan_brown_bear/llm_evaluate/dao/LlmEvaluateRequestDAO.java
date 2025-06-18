d0ba84063a72e918f4d24b29b508fcc612ba9585
taiwan-brown-bear <charles.liu.0857@gmail.com>
2025-06-18 15:51:52 -0700

    step_06

--- src/main/java/com/taiwan_brown_bear/llm_evaluate/dao/LlmEvaluateRequestDAO.java
+++ src/main/java/com/taiwan_brown_bear/llm_evaluate/dao/LlmEvaluateRequestDAO.java
@@ -6,6 +6,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
 
 @Entity // This tells Hibernate to make a table out of this class
 @Table(name = "llm_evaluate_requests")
@@ -18,6 +22,11 @@ public class LlmEvaluateRequestDAO {
 
     @Column(length = 1000)
     private String systemMessage;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    //private LocalDateTime createdAt; // Use LocalDateTime for modern Java applications e.g., 2025-06-18 15:45:02.184259
+    private Instant createdOn;// e.g., 2025-06-18 22:48:58.381838 <- UTC time. The above is Bay Area time.
 
     public Integer getRequestId() {
         return requestId;
