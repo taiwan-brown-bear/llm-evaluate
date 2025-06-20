5ae306802cc62c16a53506053b4ddf36ea93d027
taiwan-brown-bear <charles.liu.0857@gmail.com>
2025-06-20 11:39:33 -0700

    step 08 2

--- src/main/java/com/taiwan_brown_bear/llm_evaluate/dto/LlmEvaluateResultDTO.java
+++ src/main/java/com/taiwan_brown_bear/llm_evaluate/dto/LlmEvaluateResultDTO.java
@@ -1,21 +1,21 @@
 package com.taiwan_brown_bear.llm_evaluate.dto;
 
-import java.util.ArrayList;
+import lombok.Builder;
+import lombok.Data;
+
 import java.util.List;
 
-public record LlmEvaluateResultDTO(
-        String targetModel,
-        String targetModelResponse,
-        String evaluatedBy,
-        String guidelineForEvaluation,
-        String evaluationResult,
-        Boolean isValid,
-        List<String> issues,
-        Double confidence) {
-    public LlmEvaluateResultDTO(String targetModel, String targetModelResponse, String evaluatedBy, String guidelineForEvaluation, String evaluationResult){
-        this(targetModel, targetModelResponse, evaluatedBy, guidelineForEvaluation, evaluationResult, null, null, null);
-    }
-    public LlmEvaluateResultDTO(String targetModel, String targetModelResponse, String issue){
-        this(targetModel, targetModelResponse, null, null, null, false, List.of(issue), null);
-    }
+@Data
+@Builder(toBuilder = true)
+public class LlmEvaluateResultDTO {
+
+    private String       targetModel;
+    private String       targetModelResponse;
+    private String       evaluatedBy;
+    private String       guidelineForEvaluation;
+    private String       evaluationResult;
+    private Boolean      isValid;
+    private List<String> issues;
+    private Double       confidence;
+
 }
--- src/main/java/com/taiwan_brown_bear/llm_evaluate/service/llm/LlmModel.java
+++ src/main/java/com/taiwan_brown_bear/llm_evaluate/service/llm/LlmModel.java
@@ -10,12 +10,16 @@ import org.springframework.ai.chat.messages.SystemMessage;
 import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.retry.NonTransientAiException;
+
 import java.util.List;
 
 @Slf4j
 public abstract class LlmModel {
 
     protected ChatClient chatClient;
+
+    private static ObjectMapper objectMapper = new ObjectMapper();
 
     public LlmEvaluateResultDTO getResponse(String systemMsg, String userMsg)
     {
@@ -28,51 +32,90 @@ public abstract class LlmModel {
         final String response = chatResponse.getResult().getOutput().getText();
 
         log.info("from model, {}, we got response, {}", model, response);
-        return new LlmEvaluateResultDTO(model, systemMsg, response);
+        return LlmEvaluateResultDTO.builder()
+                .targetModel(model)
+                .targetModelResponse(response)
+                .build();
     }
 
     public LlmEvaluateResultDTO getEvaluationResult(String originalPrompt, String response)
     {
-        String promptToEvaluate =
-                LlmPromptSystemMessageTemplate.getGuidelineForEvaluation(
-                        originalPrompt,
-                        response,
-                        LlmPromptSystemMessageTemplate.EvaluationResult.JSON_FORMAT);
-
+        // step 1: prepare the prompt
+        String promptToEvaluate = LlmPromptSystemMessageTemplate.getGuidelineForEvaluation(originalPrompt, response, LlmPromptSystemMessageTemplate.EvaluationResult.JSON_FORMAT);
         log.debug("promptToEvaluate: " + promptToEvaluate);
+        final String evalSystemMsg = promptToEvaluate;
+        final String evalUserMsg   = promptToEvaluate;
 
-        String systemMsg = promptToEvaluate;
-        String userMsg   = promptToEvaluate;
+        // step 2: ask llm to evaluate
+        SystemMessage evaluationSystemMessage = new SystemMessage(evalSystemMsg == null ? "" : evalSystemMsg);
+        UserMessage   evaluationUserMessage   = new UserMessage  (evalUserMsg);
+        Prompt prompt = new Prompt(List.of(evaluationSystemMessage, evaluationUserMessage));
 
-        SystemMessage systemMessage = new SystemMessage(systemMsg == null ? "" : systemMsg);
-        UserMessage userMessage = new UserMessage(userMsg);
-        Prompt prompt = new Prompt(List.of(systemMessage, userMessage));
-        ChatResponse chatResponse = chatClient.prompt(prompt).call().chatResponse();
+        ChatResponse evaluationModelChatResponse = null;
+        try {
+            evaluationModelChatResponse = chatClient.prompt(prompt).call().chatResponse();
+        } catch (NonTransientAiException e) {
+            log.warn("might exceed the allowed rate ...", e);
+            return LlmEvaluateResultDTO.builder()
+                    .guidelineForEvaluation(evalSystemMsg)
+                    .issues(List.of(e.getMessage()))
+                    .build();
+        } finally {
+            log.debug("evaluationModelChatResponse: {}", evaluationModelChatResponse);
+        }
 
-        final String evaluationModel         = chatResponse.getMetadata().getModel();
-        final String evaluationModelResponse = chatResponse.getResult().getOutput().getText();
+        // step 3: get eval. response
+        final String evaluationModel         = evaluationModelChatResponse.getMetadata().getModel();
+        String evaluationModelResponse = evaluationModelChatResponse.getResult().getOutput().getText();
 
-        ObjectMapper objectMapper = new ObjectMapper();
+        // step 4: parse the json response
         LlmPromptSystemMessageTemplate.EvaluationResult evaluationResult = null;
         try {
             evaluationResult = objectMapper.readValue(evaluationModelResponse, LlmPromptSystemMessageTemplate.EvaluationResult.class);
-        } catch (JsonProcessingException jsonProcessingException) {
-            log.warn("failed to parse the response, " + evaluationModelResponse);
+        } catch (JsonProcessingException e) {
+
+            boolean isSuc = false;
+            try {
+            // for Anthropic,
+            // "```json\n{\n    \"isValid\": true,\n    \"issues\": [],\n    \"confidence\": 0.98\n}\n```"
+            if(evaluationModelResponse != null               &&
+               evaluationModelResponse.startsWith("```json") &&
+               evaluationModelResponse.endsWith  ("```")     ){
+                evaluationModelResponse = evaluationModelResponse.substring("```json".length());
+                evaluationModelResponse = evaluationModelResponse.substring(0, evaluationModelResponse.length() - "```".length());
+                evaluationResult = objectMapper.readValue(evaluationModelResponse, LlmPromptSystemMessageTemplate.EvaluationResult.class);
+                isSuc = true;
+            }
+            } catch (JsonProcessingException e1) {
+                log.warn("abc failed to parse the response, {}", evaluationModelResponse, e1);
+            }
+            if(!isSuc) {
+
+                log.warn("failed to parse the response, {}", evaluationModelResponse, e);
+                return LlmEvaluateResultDTO.builder()
+                        .evaluatedBy(evaluationModel)
+                        .guidelineForEvaluation(evalSystemMsg)
+                        .evaluationResult(evaluationModelResponse)
+                        .issues(List.of(e.getMessage()))
+                        .build();
+            }
+        } finally {
+
+
+
+
+
+            log.debug("evaluationResult: {}", evaluationResult);
         }
 
-        log.debug("evaluationResult: {}", evaluationResult);
-        if(evaluationResult == null) {
-            return new LlmEvaluateResultDTO(evaluationModel, systemMsg, evaluationModelResponse);
-        }
-        return new LlmEvaluateResultDTO(
-                null,
-                null,
-                evaluationModel,
-                systemMsg,
-                evaluationModelResponse,
-                evaluationResult.getIsValid(),
-                evaluationResult.getIssues(),
-                evaluationResult.getConfidence()
-                );
+        // step 5: result the result
+        return LlmEvaluateResultDTO.builder()
+                .evaluatedBy           (evaluationModel)
+                .guidelineForEvaluation(evalSystemMsg)
+                .evaluationResult      (evaluationModelResponse)
+                .isValid               (evaluationResult.getIsValid())
+                .issues                (evaluationResult.getIssues())
+                .confidence            (evaluationResult.getConfidence())
+                .build();
     }
 }
